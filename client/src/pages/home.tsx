1c2bf764c796457370d3e4277ad68d717cae7582
AR bites. <ar.bites.tech@gmail.com>
2025-07-12 17:42:11 +0300

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -117,7 +117,7 @@ export default function Home() {
         </div>
         
         {/* Category Icons Bottom */}
-        <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex gap-8">
+        <div className="absolute bottom-24 left-1/2 transform -translate-x-1/2 flex gap-8">
           {categories.map((category) => {
             const IconComponent = category.icon;
             return (
